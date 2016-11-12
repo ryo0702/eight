30eb63ace0656928ff7117ba88763b1328229841
ryo0702 <ryo0702@gmail.com>
2016-11-13 01:40:30 +0900

    v 0.0.4
    .list-menu
    .dropdown-menu

--- js/eight.js
+++ js/eight.js
@@ -4,6 +4,32 @@ function navbar(){
   });
 }
 
+function dropdown_menu(){
+  var check_dropdown = "off";
+  var ul_ele;
+  $(".dropdown-menu").on('click','.label', function(event) {
+    var dd_height = $(this).parent().height();
+    var dd_height2 = dd_height / 2;
+    var label_height = $(this).height();
+    var dd_height3 = dd_height2 + label_height;
+    ul_elem = $(this).next("ul");
+    $(ul_elem).css('top',dd_height3 + "px");
+    // Down
+    $(ul_elem).slideDown(100);
+    $(".overlay").show();
+    check_dropdown = "on";
+  });
+
+  $('.overlay').on('click', function() {
+    if(check_dropdown == "on"){
+      $(".overlay").hide();
+      $(ul_elem).slideUp(50);
+      check_dropdown = "off";
+    }
+  });
+
+}
+
 function main_margin_fixnavbar() {
   var nav_height = $(".navbar").height();
   $("main").css('margin-top',nav_height);
@@ -22,7 +48,7 @@ function escape_html_code(str) {
 }
 
 function codes(){
-  $("code").each(function(i,elem){
+  $("code,.code").each(function(i,elem){
     var code_html = $(elem).html();
     var escape_html = escape_html_code(code_html);
     $(elem).html(escape_html);
@@ -30,9 +56,8 @@ function codes(){
 }
 
 $(document).ready(function() {
-  // Ui
   navbar();
   main_margin_fixnavbar();
   billboard_subtract_fixnavbar();
-  codes();
+  dropdown_menu();
 });
