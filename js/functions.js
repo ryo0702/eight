01e3efa055fb771eb373e6274957f2d4920759fc
ryo0702 <ryo0702@gmail.com>
2016-11-12 19:45:48 +0900

    ver 0.0.3
    jquery routing function.
    rename html file.

--- js/functions.js
+++ js/functions.js
@@ -1,4 +1,4 @@
-function ui_set(route){
+function html_construction(route){
   $("#navbar").load(route + "parts/navbar.html");
   $("#sidebar").load(route + "parts/sidebar.html");
   $("#footer").load(route + "parts/footer.html");
@@ -7,8 +7,14 @@ function ui_set(route){
 function replace_url(route){
   if(route == ''){
     $('a.replace_url').each(function(i,elem){
-      var $href = $(this).attr('href');
+      var href = $(this).attr('href');
       $(this).attr("href","document/" + href);
+    });
+  }
+  else{
+    $('a.replace_url_up').each(function(i,elem){
+      var href = $(this).attr('href');
+      $(this).attr("href","../"+href);
     });
   }
 }
